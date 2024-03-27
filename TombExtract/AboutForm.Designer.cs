6c80b5c88b5193183a19f948791ecd67575b0c49
Julian Ozel Rose <julian.ozel.rose@outlook.com>
2024-03-27 15:21:48 -0700

    Update version to 1.17

--- TombExtract/AboutForm.Designer.cs
+++ TombExtract/AboutForm.Designer.cs
@@ -81,7 +81,7 @@ namespace TombExtract
             this.lblVersionNumber.Name = "lblVersionNumber";
             this.lblVersionNumber.Size = new System.Drawing.Size(39, 16);
             this.lblVersionNumber.TabIndex = 4;
-            this.lblVersionNumber.Text = "v1.15";
+            this.lblVersionNumber.Text = "v1.17";
             // 
             // lblAuthorName
             // 
